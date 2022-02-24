a9e7873059b962af2f17857bb1538380046ff010
xlsea <m@xlsea.cn>
2022-02-24 15:20:12 +0800

    初始化提交

--- src/main/java/cn/xlsea/influx/dao/Influx.java
+++ src/main/java/cn/xlsea/influx/dao/Influx.java
@@ -48,7 +48,7 @@ public interface Influx {
      * 查询数据
      *
      * @param clazz 实体对象,对象集合(对应实体类必须使用@Tag注解指定一个字段)
-     * @param sql 实体对象,对象集合(对应实体类必须使用@Tag注解指定一个字段)
+     * @param sql 查询语句
      * @param str 表名后需要添加的文本
      * @retuen T or List<T> 返回集合或对象实体
      */
