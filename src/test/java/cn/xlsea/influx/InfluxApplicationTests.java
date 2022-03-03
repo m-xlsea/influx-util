6c7e11c72b359bf2132b17f09f9abc50db026acd
xlsea <m@xlsea.cn>
2022-03-03 16:04:18 +0800

    初始化提交

--- src/test/java/cn/xlsea/influx/InfluxApplicationTests.java
+++ src/test/java/cn/xlsea/influx/InfluxApplicationTests.java
@@ -5,32 +5,131 @@ import cn.xlsea.influx.annotation.Tag;
 import cn.xlsea.influx.dao.Influx;
 import org.influxdb.annotation.Measurement;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @SpringBootTest
 class InfluxApplicationTests {
 
+    Logger log = LoggerFactory.getLogger(InfluxApplicationTests.class);
+
     @Resource
     private Influx influx;
 
+    /**
+     * 测试连接
+     */
     @Test
-    void save() {
-        User user = new User("111111", "张三", "男", 20, new Date(), new Date());
-        int save = influx.save(user);
-        System.out.println(save);
+    void testPing() {
+        // 连接成功返回true，失败返回false
+        Boolean ping = influx.ping();
+        if (ping) {
+            log.info("连接成功");
+        } else {
+            log.info("连接失败");
+        }
     }
 
+    /**
+     * 判断数据库是否存在
+     */
     @Test
-    void list() {
+    void testExists() {
+        // 无参数则创建配置类里的数据库
+        // Boolean exists = influx.exists();
+        Boolean exists = influx.exists("utilTest");
+        if (exists) {
+            log.info("数据库存在");
+        } else {
+            log.info("数据库不存在");
+        }
+    }
+
+    /**
+     * 创建数据库
+     */
+    @Test
+    void testCreateDataBase() {
+        // 无参数则创建配置类里的数据库
+        // String dataBase = influxDao.createDataBase();
+        String dataBase = influx.createDataBase("utilTest");
+        log.info(dataBase);
+    }
+
+    /**
+     * 删除数据库
+     */
+    @Test
+    void testDeleteDataBase() {
+        // 无参数则删除配置类里的数据库
+        // String dataBase = influxDao.deleteDataBase();
+        String dataBase = influx.deleteDataBase("utilTest");
+        log.info(dataBase);
+    }
+
+    /**
+     * 插入数据
+     */
+    @Test
+    void testSave() {
+        List<User> users = new ArrayList<>();
+        Date date = new Date();
+        User user = new User("1", "张三", "男", 18, date, date);
+        User user2 = new User("2", "李四", "女", 16, date, date);
+        User user3 = new User("3", "王五", "男", 20, date, date);
+        users.add(user);
+        users.add(user2);
+        users.add(user3);
+        // 参数可以是实体对象，也可以是对象集合
+        int insertOne = influx.save(user);
+        int insertAll = influx.save(users);
+        // 可以在表名后添加额外文本
+        influx.save(user);
+        log.info("影响行数：" + insertOne);
+        log.info("影响行数：" + insertAll);
+    }
+
+    /**
+     * 查询多条数据
+     */
+    @Test
+    void testList() {
+        // 查询语句
         String sql = "select * from userInfo";
-        List<User> list = influx.list(User.class, sql);
-        System.out.println(list);
+        // 参数为需要返回的对象类型和语句
+        List<User> query = influx.list(User.class, sql);
+        log.info(query.toString());
+    }
+
+    /**
+     * 查询单条数据
+     */
+    @Test
+    void testGetOne() {
+        // 查询语句
+        String sql = "select * from userInfo limit 1";
+        // 参数为需要返回的对象类型和语句
+        User query = influx.getOne(User.class, sql);
+        log.info(query.toString());
+    }
+
+    /**
+     * 查询记录数
+     */
+    @Test
+    void testCount() {
+        // 查询语句
+        String sql = "select count(age) from userInfo";
+        // 参数为需要返回的对象类型和语句
+        int query = influx.count(User.class, sql);
+        log.info("共查到" + query + "条数据");
     }
 
     @Measurement(name = "userInfo")
@@ -115,6 +214,5 @@ class InfluxApplicationTests {
             return "User{id=" + id + ", names=" + names + ", sex=" + sex + ", age=" + age + ", createTime=" + createTimeStr + ", updateTime=" + updateTimeStr + "}";
         }
     }
-
 
 }
